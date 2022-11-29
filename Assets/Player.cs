2bc3f4ebc7244bcc57f830b44c36c0753d69a63e
Taiga <k020g1219@g.neec.ac.jp>
2022-11-29 17:40:35 +0900

    移動+背景+アニメ追加

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -5,28 +5,42 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
 
-    public float speed = 8.0f;
-
+    public float speed = 4.0f;
+    public float power = 6.0f;
     public Rigidbody rb;
 
-    float moveX = 0.0f;
-    float moveZ = 0.0f;
+    private float x;
+    private float z;
+    private Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();
     }
 
-   // Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
-        moveX = Input.GetAxis("Horizontal") * speed;
-        moveZ = Input.GetAxis("Vertical") * speed;
-        Vector3 dir = new Vector3(moveX, 0, moveZ);
+        x = Input.GetAxisRaw("Horizontal");
+        z = Input.GetAxisRaw("Vertical");
+     
+        Vector3 dir = new Vector3(x, 0, z);
+
+        rb.velocity = new Vector3(x, 0, z) * speed;
+        animator.SetFloat("run", rb.velocity.magnitude);
+
+        if (dir.magnitude > 0.1)
+        {
+            Quaternion rot = Quaternion.LookRotation(dir);
+            transform.rotation =
+                Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * 10f);
+        }
     }
 
     void FixedUpdate()
     {
-      rb.velocity = new Vector3(moveX, 0, moveZ);
+
     }
 }
